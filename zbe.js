7382e52d06b8c1d42f8bcc579e50ef7de063bfda
siefaldin123 <41337660+siefaldin123@users.noreply.github.com>
2020-02-09 06:01:18 +0200

    Create zbe.js

---
+++ zbe.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("message", message => {
+  if (message.channel.type === "dm") {
+    if (message.author.id === client.user.id) return;
+    var embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setTimestamp()
+      .setTitle("متحرش جديد")
+      .setThumbnail(`${message.author.avatarURL}`)
+      .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+      .setFooter(`From **${message.author.tag} (${message.author.id})**`);
+    client.channels.get("675900837813288960").send({ embed: embed });
+  }
+});
+
+client.login(token);
